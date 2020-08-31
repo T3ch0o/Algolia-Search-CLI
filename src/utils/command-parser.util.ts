9edb9d8b1cfdfaf23889641557ea8bea21882ee4
Techø <t3cho.k@gmail.com>
2020-09-01 00:28:16 +0300

    [command-parser.util] Add getCommand method.

--- src/utils/command-parser.util.ts
+++ src/utils/command-parser.util.ts
@@ -2,4 +2,8 @@ export class CommandParser {
     public static collectInputData(value: string): Array<string> {
         return value.split(':');
     }
+
+    public static getCommand(cli: any): string {
+        return cli.rawArgs[2].substring(2);
+    }
 }
