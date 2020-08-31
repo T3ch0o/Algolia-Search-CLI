b0a353bf446e583c070de051b9f13b3fcdf1da78
Techø <t3cho.k@gmail.com>
2020-09-01 01:27:04 +0300

    [handlers/injector.config] Add ClearCacheHandler.

--- src/engine/handlers/injector.config.ts
+++ src/engine/handlers/injector.config.ts
@@ -1,5 +1,6 @@
+import { ClearCacheHandler } from './commands/clear-cache.handler';
 import { SetEnvHandler } from './commands/set-env.handler';
 
 export const handlers = [
-    SetEnvHandler
+    SetEnvHandler, ClearCacheHandler
 ];
