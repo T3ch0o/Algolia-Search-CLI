6d0f9334c6d839e46152a070eb99f2b29ae223cd
Techø <t3cho.k@gmail.com>
2020-09-01 00:23:26 +0300

    [logger] Add different logging types.

--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -1,5 +1,6 @@
 import { Settings } from '../config/settings';
 import figlet from 'figlet';
+import chalk from 'chalk';
 import { Injectable } from 'injection-js';
 
 @Injectable()
@@ -9,6 +10,22 @@ export class Logger {
 
     public printIntroduction(): void {
         console.log(figlet.textSync('Algolia-Search-CLI', { horizontalLayout: 'full' }) + '\n');
-        this.settings.command.outputHelp();
+        this.settings.cli.outputHelp();
+    }
+
+    public error(message: string): void {
+        console.log(chalk.red(message));
+    }
+
+    public info(message: string): void {
+        console.log(chalk.blueBright(message));
+    }
+
+    public warn(message: string): void {
+        console.log(chalk.yellow(message));
+    }
+
+    public success(message: string): void {
+        console.log(chalk.green(message));
     }
 }
