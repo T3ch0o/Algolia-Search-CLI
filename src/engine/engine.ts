3a390c079e7cf36431febef2c39e3d3f293ebd99
Techø <t3cho.k@gmail.com>
2020-09-01 00:26:48 +0300

    [engine] Add engine init and route method.

---
+++ src/engine/engine.ts
@@ -0,0 +1,34 @@
+import { Injectable, Injector } from "injection-js";
+import { CommandHandler } from "./handlers/command.handler";
+import { handlers } from './handlers/injector-config.handlers';
+
+@Injectable()
+export class Engine {
+    private map: Map<string, CommandHandler>;
+
+    constructor(private injector: Injector) { }
+
+    /**
+     * Filling our map with the command handlers that will handle our command execution
+     */
+    public init(): void {
+        this.map = new Map<string, CommandHandler>();
+
+        handlers.forEach((id: any) => {
+            const service = this.injector.get(id);
+
+            if (service instanceof CommandHandler) {
+                this.map.set(service.getMethod(), service);
+            }
+        });
+    }
+
+    /**
+     * Route the command to the appropriate Handler
+     *
+     * @param command
+     */
+    public route(command: string) {
+        this.map.get(command).handle();
+    }
+}
