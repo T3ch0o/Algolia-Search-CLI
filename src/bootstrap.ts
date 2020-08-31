df788c3f6767d4d00af9bf95fbdb86baa4b01b73
Techø <t3cho.k@gmail.com>
2020-09-01 00:22:59 +0300

    [boostrap] Add engine init and routing to the right command.

---
+++ src/bootstrap.ts
@@ -0,0 +1,26 @@
+import { Command } from './enums/commands.enum';
+import { Engine } from './engine/engine';
+import { Logger } from './utils/logger.util';
+import { Injectable } from "injection-js";
+
+import { Settings } from './config/settings';
+
+@Injectable()
+export class Bootstrap {
+    constructor(private settings: Settings, private logger: Logger, private engine: Engine) {
+    }
+
+    /**
+     * Setup/proccess current command
+     */
+    public start(): void {
+        this.settings.init();
+
+        if (process.argv.length < 3) {
+            this.logger.printIntroduction();
+        }
+
+        this.engine.init();
+        this.engine.route(this.settings.command);
+    }
+}
