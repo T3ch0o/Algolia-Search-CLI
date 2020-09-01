040f117c2a3f4a1f3888f11bcd428d19c9a4c647
Techø <t3cho.k@gmail.com>
2020-09-01 18:32:01 +0300

    [injector.config] Add SearchHandler.

--- src/engine/handlers/injector.config.ts
+++ src/engine/handlers/injector.config.ts
@@ -1,6 +1,7 @@
 import { ClearCacheHandler } from './commands/clear-cache.handler';
 import { SetEnvHandler } from './commands/set-env.handler';
+import { SearchHandler } from './commands/search.handler';
 
 export const handlers = [
-    SetEnvHandler, ClearCacheHandler
+    SetEnvHandler, ClearCacheHandler, SearchHandler
 ];
